4c0ea5c006417de9d9a40f29ba4254c0e5b01f92
NicolasLegrand <nicolas1.legrand@gmail.com>
2014-02-04 10:50:17 +0000

    Accueil fonctionnel 04/02/2014

---
+++ index.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<div id='contener_logo'>
+		<img src="images/logo_ARC.png">
+	</div>
+		<div id='contener'>
+
+			<ul id='bloc_accueil'>
+				<li>
+					<a href="#">
+						<div class='icon'><span class='LigatureSymbols'>home</span></div>
+						<div class='content'><h2>Accueil</h2></div>	
+					</a>
+				</li>
+			</ul>
+
+			<ul id='bloc_prestations'>
+				<li>
+					<a href="#">
+						<div class='icon'><span class='LigatureSymbols'>memo</span></div>
+						<div class='content'><h2>Prestations</h2></div>
+					</a>	
+				</li>
+			</ul>
+			
+			<ul id='bloc_produits'>
+				<li>
+					<a href="#">
+						<div class='icon'><span class='LigatureSymbols'>server</span></div>
+						<div class='content'><h2>Produits</h2></div>
+					</a>	
+				</li>
+			</ul>
+
+			<ul id='bloc_partenaires'>
+				<li>
+					<a href="#">
+						<div class='icon'><span class='LigatureSymbols'>group</span></div>
+						<div class='content'><h2>Partenaires</h2></div>
+					</a>	
+				</li>
+			</ul>
+
+			<ul id='bloc_contact'>			
+				<li>
+					<a href="#">
+						<div class='icon'><span class='LigatureSymbols'>mail</span></div>
+						<div class='content'><h2>Contact</h2></div>
+					</a>	
+				</li>
+			</ul>
+
+			<ul id='bloc_webmail'>
+				<li>
+					<a href="#">
+						<div class='icon'><span class='LigatureSymbols'>memo</span></div>
+						<div class='content'><h2>Webmail</h2></div>
+					</a>	
+				</li>
+			</ul>
+		</div>
+</body>
+</html>
